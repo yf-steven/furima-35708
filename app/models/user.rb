cfdd741536c2e004959920a29e4bee640cf35476
yf-steven <music_beats_spirit@icloud.com>
2021-06-01 17:53:26 +0900

    passwordのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  with_options presence: true do
-   validates :nickname
-   validates :password,
+  
+  validates :password,
              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+  with_options presence: true do
+   validates :nickname   
    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
    validates :birthday
-  end
+  end  
 end

4fc9ac726a9c562c0884eee45fb416d97b8dc4cb
yf-steven <music_beats_spirit@icloud.com>
2021-06-02 17:01:18 +0900

    Active Hashの導入と定義
    バリデーションも修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,26 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
-    validates :category_id
-    validates :status_id
-    validates :cost_who_id
-    validates :post_from_id
-    validates :days_to_post_id
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :cost_who_id, numericality: { other_than: 1 }
+    validates :post_from_id, numericality: { other_than: 1 }
+    validates :days_to_post_id, numericality: { other_than: 1 }
     validates :price, format: { with: /\A[0-9]+\z/ }
   end
+
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :cost_who
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :post_from
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days_to_post
+
 end

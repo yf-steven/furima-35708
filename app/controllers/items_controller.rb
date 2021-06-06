d25114d622f23ab6727f87f6a1a28b8c09e2a6fc
yf-steven <music_beats_spirit@icloud.com>
2021-06-06 17:59:33 +0900

    アクションの安全性の向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :destroy, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :destroy, :edit, :update]
-  before_action :move_to_index, only: [:destroy, :edit]
+  before_action :move_to_index, only: [:destroy, :edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: 'DESC')

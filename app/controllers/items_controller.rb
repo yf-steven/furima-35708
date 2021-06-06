f1450f74803fc09ed45ca5526b950e3cd817746c
yf-steven <music_beats_spirit@icloud.com>
2021-06-06 15:14:55 +0900

    投稿商品削除機能の実装
    不備があった一覧表示も修正済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: [:destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: 'DESC')
@@ -22,10 +23,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :cost_who_id, :post_from_id, :days_to_post_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
   end
 end
